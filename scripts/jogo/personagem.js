4217465caa97417f4f93b1aad08523c1eb8f93f0
William Goncalves <euwilliamgoncalves@gmail.com>
2020-06-23 23:30:34 -0300

    Adicionando colisão frontal e novo inimigo

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,5 +1,47 @@
 class Personagem extends Animacao {
   constructor(matriz, imagem, x, y, largura, altura, larguraSprite, alturaSprite){
       super(matriz, imagem, x, y, largura, altura, larguraSprite, alturaSprite);
+
+    this.yInicial = y;
+    //this.y = this.yInicial;
+
+    this.imagemPadrao = imagem;
+    this.imgJump = loadImage("imagens/personagem/jump.png");
+    
+    this.velocidadeDoPulo = 0;
+    this.gravidade = 3;
+
+    this.colidiu = false;
+
+    
   }
+  
+  pula() {
+    this.imagem = this.imgJump;
+    this.velocidadeDoPulo = -30
+  }
+  
+  aplicaGravidade() {
+    this.y = this.y + this.velocidadeDoPulo
+    this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade
+    
+    if(this.y > this.yInicial){
+      this.y = this.yInicial
+      this.imagem = this.imagemPadrao;
+    }
+  }
+
+  estaColidindo() {
+    rect(this.x + 130, this.y + 70, 2, this.altura / 3);
+    rect(enemy.x + 8, enemy.y + 5, 3, enemy.altura);
+    const colisao = collideRectRect(this.x + 130, this.y + 70, 2, this.altura / 3, enemy.x + 8, enemy.y, 3, enemy.altura);
+
+    if (colisao == true && this.colidiu == false) {
+      playerEnergy -= 1;
+      console.log(playerEnergy);
+    }
+    
+    
+  }
+
 }
--- sketch.js
+++ sketch.js
@@ -4,11 +4,12 @@ let cenario;
 let personagem;
 let player;
 let playerSprite = 128;
+let playerEnergy = 5;
 
 let inimigo;
 let enemy;
-let enemySpriteX = 105;
-let enemySpriteY = 100;
+let enemySpriteX = 22;
+let enemySpriteY = 33;
 
 let somDoJogo;
 
@@ -32,48 +33,33 @@ const matrizPersonagem = [
 ];
 
 const matrizInimigo = [
-  [0, 0],
-  [enemySpriteX, 0],
-  [enemySpriteX*2, 0],
+  [enemySpriteX*12, 0],
+  [enemySpriteX*11, 0],
+  [enemySpriteX*10, 0],
+  [enemySpriteX*9, 0],
+  [enemySpriteX*8, 0],
+  [enemySpriteX*7, 0],
+  [enemySpriteX*6, 0],
+  [enemySpriteX*5, 0],
+  [enemySpriteX*4, 0],
   [enemySpriteX*3, 0],
-  [0, enemySpriteY],
-  [enemySpriteX, enemySpriteY],
-  [enemySpriteX*2, enemySpriteY],
-  [enemySpriteX*3, enemySpriteY],
-  [0, enemySpriteY*2],
-  [enemySpriteX, enemySpriteY*2],
-  [enemySpriteX*2, enemySpriteY*2],
-  [enemySpriteX*3, enemySpriteY*2],
-  [0, enemySpriteY*3],
-  [enemySpriteX, enemySpriteY*3],
-  [enemySpriteX*2, enemySpriteY*3],
-  [enemySpriteX*3, enemySpriteY*3],
-  [0, enemySpriteY*4],
-  [enemySpriteX, enemySpriteY*4],
-  [enemySpriteX*2, enemySpriteY*4],
-  [enemySpriteX*3, enemySpriteY*4],
-  [0, enemySpriteY*5],
-  [enemySpriteX, enemySpriteY*5],
-  [enemySpriteX*2, enemySpriteY*5],
-  [enemySpriteX*3, enemySpriteY*5],
-  [0, enemySpriteY*6],
-  [enemySpriteX, enemySpriteY*6],
-  [enemySpriteX*2, enemySpriteY*6],
-  [enemySpriteX*3, enemySpriteY*6]
+  [enemySpriteX*2, 0],
+  [enemySpriteX*1, 0],
+  [0, 0]
 ];
 
 function preload() {
    fundo = loadImage("imagens/cenario/fundo.jpg");
    personagem = loadImage("imagens/personagem/arqueiro.png");
-   inimigo = loadImage("imagens/inimigos/gotinha.png")
+   inimigo = loadImage("imagens/inimigos/bones.png")
    somDoJogo = loadSound("sons/trilha_jogo.mp3");
 }
 
 function setup() {
   createCanvas(720, 480);
-  cenario = new Cenario(fundo, 5);
+  cenario = new Cenario(fundo, 6);
   player = new Personagem(matrizPersonagem, personagem, -20, 290, playerSprite*2, playerSprite*2, playerSprite, playerSprite);
-  enemy = new Inimigo(matrizInimigo, inimigo, width-60, 417, 32, 32, enemySpriteX, enemySpriteY)
+  enemy = new Inimigo(matrizInimigo, inimigo, width-60, 362, enemySpriteX * 2.5, enemySpriteY * 2.5, enemySpriteX, enemySpriteY)
   frameRate(30);
   //somDoJogo.loop();
 }
@@ -82,7 +68,14 @@ function draw() {
   cenario.exibe();
   cenario.move();
   player.exibe();
+  player.aplicaGravidade();
   enemy.exibe();
   enemy.move();
+  player.estaColidindo(enemy);
 }
 
+function keyPressed() {
+  if (key === 'ArrowUp') {
+    player.pula();
+  }
+}
